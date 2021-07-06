aea30b0ec37cd39b2a090135ae2d9b7b820a668b
chetna1913195 <saxenachetna30@gmail.com>
2021-07-06 13:05:50 +0530

    created private and public page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import './styles/main.scss'
+import React from 'react';
+import './styles/main.scss';
 import 'rsuite/dist/styles/rsuite-default.css';
+import { Switch, Route } from 'react-router';
+import Signin from './pages/Signin';
+import Home from './pages/Home';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <Switch>
+      <Route path="/signin">
+        <Signin />
+      </Route>
+      <PrivateRoute path="/ ">
+        <Home />
+      </PrivateRoute>
+    </Switch>
   );
 }
 
---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCqt8-QuRfCqYqIdZXINiDPg-U8C49a_j0',
+  authDomain: 'chat-web-app-f5ac7.firebaseapp.com',
+  projectId: 'chat-web-app-f5ac7',
+  storageBucket: 'chat-web-app-f5ac7.appspot.com',
+  messagingSenderId: '648954959063',
+  appId: '1:648954959063:web:7e0d946f8e899492f40c92',
+};
+
+const app = firebase.initializeApp(config);
